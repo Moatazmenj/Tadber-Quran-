dab63d2a911657f188b012f054d8cde5be9c9805
musul mans <mansmusul@gmail.com>
2025-07-06 15:21:21 +0000

    انزع بلد فرنسا

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -5,6 +5,5 @@ export const translationOptions: TranslationOption[] = [
     { id: 'ur', apiId: 819, language: 'Urdu', nativeName: 'اردو', translator: 'Fateh Muhammad Jalandhry', flag: '🇵🇰' },
     { id: 'id', apiId: 33, language: 'Bahasa Indonesia', nativeName: 'Bahasa Indonesia', translator: 'Indonesian Ministry of Religious Affairs', flag: '🇮🇩' },
     { id: 'tr', apiId: 44, language: 'Türkçe', nativeName: 'Türkçe', translator: 'Diyanet Isleri', flag: '🇹🇷' },
-    { id: 'fr', apiId: 22, language: 'Français', nativeName: 'Français', translator: 'Muhammad Hamidullah', flag: '🇫🇷' },
     { id: 'en', apiId: 131, language: 'English', nativeName: 'English', translator: 'Saheeh International', flag: '🇬🇧' },
 ];
