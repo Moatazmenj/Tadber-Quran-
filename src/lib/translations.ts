21049fd6a4be5dd5d82d25d25791d979a0efb141
musul mans <mansmusul@gmail.com>
2025-07-17 08:29:02 +0000

    انزع اللغة بنغلادشية

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -5,5 +5,4 @@ export const translationOptions: TranslationOption[] = [
     { id: 'fr', apiId: 136, language: 'French', nativeName: 'Français', translator: 'Muhammad Hamidullah', flag: '🇫🇷' },
     { id: 'ur', apiId: 819, language: 'Urdu', nativeName: 'اردو', translator: 'Fateh Muhammad Jalandhry', flag: '🇵🇰' },
     { id: 'tr', apiId: 44, language: 'Turkish', nativeName: 'Türkçe', translator: 'Diyanet Isleri', flag: '🇹🇷' },
-    { id: 'bn', apiId: 61, language: 'Bengali', nativeName: 'বাংলা', translator: 'Taisirul Quran', flag: '🇧🇩' },
 ];
