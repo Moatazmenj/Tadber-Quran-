227c7c0ef48d16fcf10ac155cc83e51a4f9505d4
musul mans <mansmusul@gmail.com>
2025-07-17 23:30:45 +0000

    انزع ترجمة المانية هندسة وصينية

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -4,11 +4,8 @@ export const translationOptions: TranslationOption[] = [
     { id: 'en', apiId: 131, language: 'English', nativeName: 'English', translator: 'Saheeh International', flag: '🇬🇧' },
     { id: 'es', apiId: 83, language: 'Spanish', nativeName: 'Español', translator: 'Raúl González Bornez', flag: '🇪🇸' },
     { id: 'fr', apiId: 20, language: 'French', nativeName: 'Français', translator: 'Muhammad Hamidullah', flag: '🇫🇷' },
-    { id: 'de', apiId: 85, language: 'German', nativeName: 'Deutsch', translator: 'A. S. F. Bubenheim & N. Elyas', flag: '🇩🇪' },
     { id: 'id', apiId: 33, language: 'Indonesian', nativeName: 'Bahasa Indonesia', translator: 'Indonesian Ministry of Religious Affairs', flag: '🇮🇩' },
     { id: 'ru', apiId: 101, language: 'Russian', nativeName: 'Русский', translator: 'Abu Adel', flag: '🇷🇺' },
     { id: 'tr', apiId: 212, language: 'Turkish', nativeName: 'Türkçe', translator: 'Diyanet Foundation', flag: '🇹🇷' },
     { id: 'ur', apiId: 819, language: 'Urdu', nativeName: 'اردو', translator: 'Fateh Muhammad Jalandhry', flag: '🇵🇰' },
-    { id: 'hi', apiId: 97, language: 'Hindi', nativeName: 'हिन्दी', translator: 'Suhel Farooq Khan and Saifur Rahman Nadwi', flag: '🇮🇳' },
-    { id: 'zh', apiId: 19, language: 'Chinese', nativeName: '中文', translator: 'Ma Jian', flag: '🇨🇳' },
 ];
