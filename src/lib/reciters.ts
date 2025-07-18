62144ad137826f10f2047f70f6319d7acf0cd020
musul mans <mansmusul@gmail.com>
2025-07-18 16:26:50 +0000

    هناك مشكلة في جلب بيانات صوتية خاصة بالماهر معقيلي

--- src/lib/reciters.ts
+++ src/lib/reciters.ts
@@ -2,7 +2,7 @@ import type { Reciter } from '@/types';
 
 export const reciters: Reciter[] = [
   { id: 7, name: 'Mishary Rashid Alafasy', style: 'Murattal', imageUrl: 'https://i.postimg.cc/FKsT9S8Q/images.jpg', server: 'Alafasy_128kbps' },
-  { id: 5, name: 'Maher Al Muaiqly', style: 'Murattal', imageUrl: 'https://i.postimg.cc/pXQvL1V5/Maher-Al-Muaiqly.jpg', server: 'maher_al_muaiqly_128kbps' },
+  { id: 5, name: 'Maher Al Muaiqly', style: 'Murattal', imageUrl: 'https://i.postimg.cc/pXQvL1V5/Maher-Al-Muaiqly.jpg', server: 'Maher_AlMuaiqly_64kbps' },
   { id: 1, name: 'AbdulBaset AbdulSamad', style: 'Mujawwad', imageUrl: 'https://i.postimg.cc/6pT50P5b/326162-1421686257.jpg', server: 'Abdul_Basit_Mujawwad_128kbps' },
   { id: 4, name: 'Abdur-Rahman as-Sudais', style: 'Murattal', imageUrl: 'https://i.postimg.cc/0j0CjLzN/abdur-rahman-as-sudais.jpg', server: 'Abdurrahmaan_As-Sudais_192kbps' },
   { id: 11, name: 'Yasser Ad-Dussary', style: 'Murattal', imageUrl: 'https://i.postimg.cc/q79pS2Xg/Yasser-Al-Dosari.jpg', server: 'Yasser_Ad-Dussary_128kbps' },
