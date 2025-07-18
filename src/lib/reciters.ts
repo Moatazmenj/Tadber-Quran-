e532eab848d155408edef4f29692764ceb676f38
musul mans <mansmusul@gmail.com>
2025-07-18 14:04:57 +0000

    قم بالستخدم مصدر قارى انجليزية موثوقة وتشتغل بكامل

--- src/lib/reciters.ts
+++ src/lib/reciters.ts
@@ -7,5 +7,5 @@ export const reciters: Reciter[] = [
   { id: 4, name: 'Abdur-Rahman as-Sudais', style: 'Murattal', imageUrl: 'https://i.postimg.cc/0j0CjLzN/abdur-rahman-as-sudais.jpg', server: 'Abdurrahmaan_As-Sudais_192kbps' },
   { id: 11, name: 'Yasser Ad-Dussary', style: 'Murattal', imageUrl: 'https://i.postimg.cc/q79pS2Xg/Yasser-Al-Dosari.jpg', server: 'Yasser_Ad-Dussary_128kbps' },
   { id: 12, name: 'Islam Sobhi', style: 'Murattal', imageUrl: 'https://i.postimg.cc/qBThT9sY/Islam-Sobhi.jpg', server: 'Islam_Sobhi_128kbps' },
-  { id: 13, name: 'Ibrahim Walk', style: 'English Translation', imageUrl: 'https://i.postimg.cc/tJ0TzJt4/ibrahim-walk.jpg', server: 'Ibrahim_Walk_192kbps' },
+  { id: 13, name: 'Ibrahim Walk', style: 'English Translation', imageUrl: 'https://i.postimg.cc/tJ0TzJt4/ibrahim-walk.jpg', server: 'ibrahim_walk_192kbps' },
 ];
