3eedf23c0e6dd92983cc6da4fb46d293d7bbd6be
musul mans <mansmusul@gmail.com>
2025-07-19 09:00:19 +0000

    قم بإضافة صورة Mishary Rashid Alafasyhttps://i.postimg.cc/9MDD90G1/misha

--- src/lib/reciters.ts
+++ src/lib/reciters.ts
@@ -1,7 +1,7 @@
 import type { Reciter } from '@/types';
 
 export const reciters: Reciter[] = [
-  { id: 7, name: 'Mishary Rashid Alafasy', style: 'Murattal', imageUrl: 'https://i.postimg.cc/FKsT9S8Q/images.jpg', server: 'Alafasy_128kbps' },
+  { id: 7, name: 'Mishary Rashid Alafasy', style: 'Murattal', imageUrl: 'https://i.postimg.cc/9MDD90G1/mishary-rashid-alafasy-profile.jpg', server: 'Alafasy_128kbps' },
   { id: 5, name: 'Maher Al Muaiqly', style: 'Murattal', imageUrl: 'https://i.postimg.cc/66h0c1RZ/OIP-2.webp', server: 'Maher_AlMuaiqly_64kbps' },
   { id: 1, name: 'AbdulBaset AbdulSamad', style: 'Mujawwad', imageUrl: 'https://i.postimg.cc/6pT50P5b/326162-1421686257.jpg', server: 'Abdul_Basit_Mujawwad_128kbps' },
   { id: 4, name: 'Abdur-Rahman as-Sudais', style: 'Murattal', imageUrl: 'https://i.postimg.cc/Hssth44h/download.webp', server: 'Abdurrahmaan_As-Sudais_192kbps' },
