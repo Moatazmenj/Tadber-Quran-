0e8a5a32488c4e7dff246ecbc8387389b7661f9e
musul mans <mansmusul@gmail.com>
2025-07-19 08:47:16 +0000

    قارئ انجليزية لا يشتغل هناك مشكلة في بيانات صوتية

--- src/lib/reciters.ts
+++ src/lib/reciters.ts
@@ -6,5 +6,4 @@ export const reciters: Reciter[] = [
   { id: 1, name: 'AbdulBaset AbdulSamad', style: 'Mujawwad', imageUrl: 'https://i.postimg.cc/6pT50P5b/326162-1421686257.jpg', server: 'Abdul_Basit_Mujawwad_128kbps' },
   { id: 4, name: 'Abdur-Rahman as-Sudais', style: 'Murattal', imageUrl: 'https://i.postimg.cc/0j0CjLzN/abdur-rahman-as-sudais.jpg', server: 'Abdurrahmaan_As-Sudais_192kbps' },
   { id: 11, name: 'Yasser Ad-Dussary', style: 'Murattal', imageUrl: 'https://i.postimg.cc/q79pS2Xg/Yasser-Al-Dosari.jpg', server: 'Yasser_Ad-Dussary_128kbps' },
-  { id: 17, name: 'Mishary Alafasy (English)', style: 'English Translation', imageUrl: 'https://i.postimg.cc/FKsT9S8Q/images.jpg', server: 'Alafasy_Translation_128kbps' },
 ];
