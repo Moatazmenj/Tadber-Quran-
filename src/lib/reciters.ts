302f84a749c866c1a5e250ba5dc60331b8248d3b
musul mans <mansmusul@gmail.com>
2025-07-19 08:58:38 +0000

    قم بإضافة صورة ياسر دوسيري https://i.postimg.cc/G2X48Y84/ab6761610000517

--- src/lib/reciters.ts
+++ src/lib/reciters.ts
@@ -5,5 +5,5 @@ export const reciters: Reciter[] = [
   { id: 5, name: 'Maher Al Muaiqly', style: 'Murattal', imageUrl: 'https://i.postimg.cc/66h0c1RZ/OIP-2.webp', server: 'Maher_AlMuaiqly_64kbps' },
   { id: 1, name: 'AbdulBaset AbdulSamad', style: 'Mujawwad', imageUrl: 'https://i.postimg.cc/6pT50P5b/326162-1421686257.jpg', server: 'Abdul_Basit_Mujawwad_128kbps' },
   { id: 4, name: 'Abdur-Rahman as-Sudais', style: 'Murattal', imageUrl: 'https://i.postimg.cc/Hssth44h/download.webp', server: 'Abdurrahmaan_As-Sudais_192kbps' },
-  { id: 11, name: 'Yasser Ad-Dussary', style: 'Murattal', imageUrl: 'https://i.postimg.cc/q79pS2Xg/Yasser-Al-Dosari.jpg', server: 'Yasser_Ad-Dussary_128kbps' },
+  { id: 11, name: 'Yasser Ad-Dussary', style: 'Murattal', imageUrl: 'https://i.postimg.cc/G2X48Y84/ab676161000051746a6628f0fcfd0b1b01bbf912.jpg', server: 'Yasser_Ad-Dussary_128kbps' },
 ];
